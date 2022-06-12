9ef57662ccc1756f008428355c4672738f2161e6
Oscar Diaz <oscargdi@hotmail.com>
2022-06-12 14:45:35 -0400

    chore: update ci script

--- utils/ci.sh
+++ utils/ci.sh
@@ -2,6 +2,9 @@
 
 set -e
 
-pipenv run ./manage.py validate_templates -b -v 2
-pipenv run ./manage.py makemigrations --check --dry-run
+# Checks templates on syntax or compile errors.
+pipenv run ./manage.py validate_templates -v 2
+# Checks migrations if missing or not up-to-date
+pipenv run ./manage.py makemigrations -v 2 --check --dry-run
+# Runs project tests
 pipenv run ./manage.py test -v 2 --no-input
