eac4a9668045dc6d7b7f6ea6316af7e02b3d9e3a
Oscar Diaz <oscargdi@hotmail.com>
2022-07-07 03:38:45 +0000

    chore: add isort to pre-commit and ci

--- utils/ci.sh
+++ utils/ci.sh
@@ -4,6 +4,8 @@ set -e
 
 # Checks unused variables and unused imports
 pipenv run autoflake . -r -c --remove-unused-variables --remove-all-unused-imports --ignore-init-module-imports
+# Checks imports sorting
+pipenv run isort . -c --profile black
 # Checks templates on syntax or compile errors.
 pipenv run ./manage.py validate_templates -v 2
 # Checks migrations if missing or not up-to-date
