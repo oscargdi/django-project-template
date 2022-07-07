a3427b0bd9959b807edcf7b215c9799c78140cef
Oscar Diaz <oscargdi@hotmail.com>
2022-07-06 22:52:23 -0500

    Add pre commit (#1)
    * chore: update dependendencies
    
    * chore: add pre-commit
    
    * chore: add autoflake to pre-commit and ci
    
    * chore: add isort to pre-commit and ci
    
    * chore: add black to pre-commit and ci
    
    * fix: install dev dependencies for ci

--- utils/ci.sh
+++ utils/ci.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+pipenv install --dev
+
+# Checks unused variables and unused imports
+pipenv run autoflake . -r -c --remove-unused-variables --remove-all-unused-imports --ignore-init-module-imports
+# Checks imports sorting
+pipenv run isort . -c --profile black
+# Checks formatting
+pipenv run black . --check
 # Checks templates on syntax or compile errors.
 pipenv run ./manage.py validate_templates -v 2
 # Checks migrations if missing or not up-to-date
